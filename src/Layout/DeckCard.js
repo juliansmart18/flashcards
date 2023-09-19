1e70295db65be0add2b0f3ceef9314b9a9cffbdf
Julian <juliansmart1996@gmail.com>
2023-09-19 15:05:05 -0400

    added functionality

--- src/Layout/DeckCard.js
+++ src/Layout/DeckCard.js
@@ -1,52 +1,60 @@
-import React, {useState, useEffect} from "react";
+import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom/cjs/react-router-dom.min";
 import { deleteDeck } from "../utils/api";
 
-function DeckCard({deck, fetchDecks}) {
+function DeckCard({ deck, fetchDecks }) {
+  const [cardsArray, setCardsArray] = useState([]);
 
-    const [cardsArray, setCardsArray] = useState([]);
+  useEffect(() => {
+    if (deck.cards && deck.cards.length > 0) {
+      const filteredCards = deck.cards.filter((card) => !("cards" in card));
+      setCardsArray(filteredCards);
+    }
+  }, [deck]);
 
-    useEffect(() => {
-        if (deck.cards && deck.cards.length > 0) {
-          const filteredCards = deck.cards.filter((card) => !("cards" in card));
-          setCardsArray(filteredCards);
-        }
-      }, [deck]);
+  const handleDeleteClick = () => {
+    const confirmDelete = window.confirm(
+      "Delete deck?\nYou will not be able to recover it."
+    );
 
-    const handleDeleteClick = () => {
-        const confirmDelete = window.confirm(
-          "Delete deck?\nYou will not be able to recover it."
-        );
-    
-        if (confirmDelete) {
-          deleteDeck(deck.id)
-            .then(() => {
-              fetchDecks();
-            })
-            .catch((error) => {
-              console.error("Error deleting deck:", error);
-            });
-        }
-      };
-      
+    if (confirmDelete) {
+      deleteDeck(deck.id)
+        .then(() => {
+          fetchDecks();
+        })
+        .catch((error) => {
+          console.error("Error deleting deck:", error);
+        });
+    }
+  };
 
-    return (
+  return (
     <div key={deck.id} className="card">
-        <div className="card-body">
-
-        <h3 className="card-title">{deck.name}</h3>
-        <h4 className="card-title">{cardsArray.length} Cards</h4>
+      <div className="card-body">
+        <div className="d-flex justify-content-between">
+          <div className="p-2">
+            <h3 className="card-title">{deck.name}</h3>
+          </div>
+          <div className="p-2">
+            <h4 className="card-title">{cardsArray.length} cards</h4>
+          </div>
+        </div>
 
         <p className="card-text">{deck.description}</p>
 
-        <Link to={`/decks/${deck.id}`} className="btn btn-secondary">View</Link>
-        <Link to={`/decks/${deck.id}/study`} className="btn btn-primary">Study</Link>
+        <Link to={`/decks/${deck.id}`} className="btn btn-secondary">
+          View
+        </Link>
+        <Link to={`/decks/${deck.id}/study`} className="btn btn-primary">
+          Study
+        </Link>
 
-        <button onClick={handleDeleteClick} className="btn btn-danger">Delete</button>
-
-        </div>
+        <button onClick={handleDeleteClick} className="btn btn-danger">
+          Delete
+        </button>
+      </div>
     </div>
-    )
+  );
 }
 
 export default DeckCard;
